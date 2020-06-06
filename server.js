893583bab2146b925e37d72b6f801668bce0f8a4
harshal.dhake <Harshal.Dhake@niceincontact.com>
2020-06-06 22:49:53 +0530

    first draft of NodeTestPro

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+app.get('/', (req, res) => {
+  //   res.sendFile(path.join(__dirname, './static/index.html'));
+  res.render('pages/index', { pageTitle: 'Welcome to NodeJS training' });
+});
+
+app.listen(port, () => {
+  console.log(`Express server is listening on Port ${port}`);
+});
