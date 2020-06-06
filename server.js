f4304c3dde0b9b0324b1618c43897499891012b0
harshal.dhake <Harshal.Dhake@niceincontact.com>
2020-06-06 22:59:48 +0530

    Second draft

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, './views'));
 
 app.get('/', (req, res) => {
   //   res.sendFile(path.join(__dirname, './static/index.html'));
-  res.render('pages/index', { pageTitle: 'Welcome to NodeJS training' });
+  res.render('pages/index', { pageTitle: 'Welcome to NodeJS training This is first draft' });
 });
 
 app.listen(port, () => {
